63cab1583d5ad8f80cd625e8e687d198643ba996
guyf@biyond.co.il <guyf@biyond.co.il>
2022-03-18 13:22:37 +0000

    change fact

--- models/write_to_db/fact.sql
+++ models/write_to_db/fact.sql
@@ -6,13 +6,13 @@
 }}
 
 with fact as (
-SELECT buyerid,caldate,sum(pricepaid),state
+SELECT dateid,sum(pricepaid),state
 from
 {{ref('sales')}} T1
 inner join 
 {{ref('users')}} T2
 on T1.buyerid=T2.userid
-group by buyerid,caldate
+group by dateid
 )
 
 select *
